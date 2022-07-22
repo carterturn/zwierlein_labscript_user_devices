32bacbfd0209e3596a28b874456c440bc974d4d3
Carter Turn <carterturn@tutanota.de>
2022-07-22 15:19:55 -0400

    Finished register_classes

--- RigolAWG/register_classes.py
+++ RigolAWG/register_classes.py
@@ -2,4 +2,6 @@ from labscript_devices import register_classes
 
 register_classes(
     'Rigol4162Controller',
-    BLACS_tab=
+    BLACS_tab='user_devices.RigolAWG.blacs_tabs.Rigol4162Tab',
+    runviewer_parser=None,
+)
