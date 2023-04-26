b80e636cc1377c6aada01fecc65d053ce155c823
Carter Turn <carterturn@tutanota.de>
2023-04-26 15:05:08 -0400

    Updated example sequence

--- RigolAWG/example_sequence.py
+++ RigolAWG/example_sequence.py
@@ -2,24 +2,15 @@ from user_devices.RigolAWG.rigol_awg_enums import *
 from user_devices.RigolAWG.labscript_devices import RigolDG4162
 
 trigger_device = None # TODO: setup a trigger
-RigolDG4162(name='test_rf', parent_device=trigger_device,
+RigolDG4162(name='test_rf', channel_1_trigger=trigger_device, channel_2_trigger=None,
             resource_str='USB0::0x1AB1::0x0641::DG4E160800425::INSTR', access_mode='usb')
 
 start()
 t = 0
 
-test_rf.channels[0]['state'].go_high()
-test_rf.channels[0]['mode'].constant(RigolDG4162EnumMode.sweep)
-test_rf.channels[0]['freq'].constant(2000.)
-test_rf.channels[0]['freq_stop'].constant(10000.)
-test_rf.channels[0]['spacing'].constant(RigolDG4162EnumSpacing.LIN)
-test_rf.channels[0]['steps'].constant(100)
-test_rf.channels[0]['time'].constant(0.01)
-test_rf.channels[0]['trigger_slope'].constant(RigolDG4162EnumTriggerSlope.POS)
-test_rf.channels[0]['trigger_source'].constant(RigolDG4162EnumTriggerSource.EXT)
-
 t = t + 0.1
-test_rf.trigger(t, 1e-4)
+
+test_rf.channel_1.sweep_output(t, 1.0, 2000., 10000., 0.01)
 
 t = t + 0.2
 
