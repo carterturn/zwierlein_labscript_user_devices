e2cde72a23c12a79c985ec5e4e28bc1f22d0be67
Carter Turn <carterturn@tutanota.de>
2023-05-17 15:12:01 -0400

    Added frequency modulation mode to RigolAWG code

--- RigolAWG/rigol_awg_enums.py
+++ RigolAWG/rigol_awg_enums.py
@@ -1,7 +1,8 @@
 from enum import Enum
 
-RigolDG4162EnumMode = Enum('Mode', ['static', 'sweep'])
+RigolDG4162EnumMode = Enum('Mode', ['static', 'sweep', 'fm_mod'])
 RigolDG4162EnumSpacing = Enum('Spacing', ['LIN', 'LOG', 'STE'])
 RigolDG4162EnumTriggerSlope = Enum('TriggerSlope', ['POS', 'NEG'])
 RigolDG4162EnumTriggerSource = Enum('TriggerSource', ['EXT', 'INT', 'MAN'])
 RigolDG4162EnumTriggerOut = Enum('TriggerOut', ['OFF', 'POS', 'NEG'])
+RigolDG4162EnumModShape = Enum('ModShape', ['SIN', 'SQU', 'TRI', 'RAMP', 'NRAMP'])
