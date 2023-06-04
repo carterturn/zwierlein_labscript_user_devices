f98a9a2a24b2b45e06d8147cd2107439ed08cdb8
Carter Turn <carterturn@tutanota.de>
2023-06-04 18:50:36 -0400

    More efficient acquisition of multiple frames

--- AlliedVisionCamera/test_code/test_sequence.py
+++ AlliedVisionCamera/test_code/test_sequence.py
@@ -11,16 +11,15 @@ if __name__ == '__main__':
     start()
     t = 0
 
-    out_0.enable(t)
-    t += 1 
-    out_0.disable(t)
-    t += 1 
-    out_0.enable(t)
-    t += 1e-5
-    t += camera_0.expose(t, 'e0', 'b0')
-    out_0.disable(t)
     t += 0.5
-    t += camera_0.expose(t, 'e0', 'd0')
-    t += 5e-3
+
+    t_light = t
+    t_trigger = t_light - 0.03
+    out_0.enable(t_light)
+    out_0.disable(t_light + 5e-3)
+    camera_0.expose(t_trigger, 'e0', 'b0')
+    t += 0.5
+    camera_0.expose(t, 'e0', 'd0')
+    t += 0.5
 
     stop(t=t)
