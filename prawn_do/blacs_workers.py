ac8ed7a6573265ba5bc73fd9fee3ebbdfca6fdd2
Carter Turn <carterturn@tutanota.de>
2023-06-07 21:36:25 -0400

    Improved documentation of prawn_do, especially for buffered programming

--- prawn_do/blacs_workers.py
+++ prawn_do/blacs_workers.py
@@ -17,6 +17,7 @@ class PrawnDOInterface(object):
         '''Sends 'cls' command, which clears the currently stored run.
         Returns response, throws serial exception on disconnect.'''
         self.conn.write(b'cls\n')
+        # Note that read_until should read until the prompt, not newline
         return self.conn.read_until(b'> ')
 
     def abort(self):
@@ -41,7 +42,7 @@ class PrawnDOInterface(object):
         '''Sends 'add' command for a single set of output bits
         Returns response, throws serial exception on disconnect.'''
         self.conn.write('add\n'.encode())
-        self.conn.write('0x{:04x}\n'.format(bit_set).encode())
+        self.conn.write('0x{:04x}\n'.format(bit_set).encode()) # Send bits as hex string
         self.conn.write('end\n'.encode())
         return self.conn.read_until(b'> ')
 
@@ -49,6 +50,7 @@ class PrawnDOInterface(object):
         '''Sends 'add' commands for each bit_set in bit_sets list. Returns response.'''
         self.conn.write('add\n'.encode())
         for bit_set in bit_sets:
+            # Send each set of bits as hex string on its own line
             self.conn.write('0x{:04x}\n'.format(bit_set).encode())
         self.conn.write('end\n'.encode())
         return self.conn.read_until(b'> ')
@@ -61,23 +63,27 @@ class PrawnDOWorker(Worker):
         self.intf = PrawnDOInterface(self.com_port)
 
     def program_manual(self, front_panel_values):
-        self.intf.abort()
-        self.intf.clear()
+        self.intf.abort() # stop current run, if it is happening
+        self.intf.clear() # clear current Pi Pico buffer
 
-        values = np.zeros(1, dtype=np.uint16)
+        values = np.zeros(1, dtype=np.uint16) # Make 16 bit unsigned integer
         for conn, value in front_panel_values.items():
+            # "Or" each bit from the front panel into the integer
             values[0] |= value << (int(conn, 16))
+        # Send to the Pi Pico
         self.intf.add(values[0])
 
         self.intf.run()
 
     def transition_to_buffered(self, device_name, h5file, initial_values, fresh):
-        self.intf.abort()
-        self.intf.clear()
+        self.intf.abort() # stop current run, if it is happening
+        self.intf.clear() # clear current Pi Pico buffer
 
         with h5py.File(h5file, 'r') as hdf5_file:
             group = hdf5_file['devices'][device_name]
             do_table = group['do_data']
+            # The data "table" contains only a single column of integers, so just convert to list
+            # Need to append an initial zero, since first output occurs immediately (before trigger)
             self.intf.add_batch([0] + list(do_table))
 
         self.intf.run()
