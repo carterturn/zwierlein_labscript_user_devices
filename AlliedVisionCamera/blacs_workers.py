8d787a70538ff86535be5429e2118c5f1e12a530
Carter Turn <carterturn@tutanota.de>
2023-06-12 23:28:04 -0400

    Bypass "with" context for vimba/camera to potentially solve issue with opening and closing Vimba instance/camera too often.

--- AlliedVisionCamera/blacs_workers.py
+++ AlliedVisionCamera/blacs_workers.py
@@ -9,41 +9,41 @@ class AlliedVisionCamera(object):
         self.camera = None
         self.exception_on_failed_shot = True
 
-        with Vimba.get_instance() as vimba:
+        self.vimba = Vimba.get_instance()
+        self.vimba.__enter__()
+
+        try:
+            self.camera = self.vimba.get_camera_by_id('DEV_' + hex(serial_number).upper())
+        except VimbaCameraError as e:
             try:
-                self.camera = vimba.get_camera_by_id('DEV_' + hex(serial_number).upper())
+                self.camera = self.vimba.get_camera_by_id('DEV_' + hex(serial_number)[2:].upper())
             except VimbaCameraError as e:
-                try:
-                    self.camera = vimba.get_camera_by_id('DEV_' + hex(serial_number)[2:].upper())
-                except VimbaCameraError as e:
-                    raise RuntimeError('Failed to open camera {}'.format(serial_number), e)
+                raise RuntimeError('Failed to open camera {}'.format(serial_number), e)
         if self.camera is None:
             raise RuntimeError('Unable to get instance of Vimba')
+
+        self.camera.__enter__()
 
     def set_attributes(self, attr_dict):
         if len(attr_dict) == 0: # No need to acquire Vimba instance for empty dict
             return
-        with Vimba.get_instance(), self.camera:
-            for k, v in attr_dict.items():
-                print('get_feature_by_name({}).set({})'.format(k, v))
-                self.camera.get_feature_by_name(k).set(v)
+        for k, v in attr_dict.items():
+            print('get_feature_by_name({}).set({})'.format(k, v))
+            self.camera.get_feature_by_name(k).set(v)
 
     def set_attribute(self, name, value):
-        with Vimba.get_instance(), self.camera:
-            print('get_feature_by_name(name).set(value)')
-            self.camera.get_feature_by_name(name).set(value)
+        print('get_feature_by_name(name).set(value)')
+        self.camera.get_feature_by_name(name).set(value)
 
     def get_attribute_names(self, visibility_level, writeable_only=True):
         from vimba.feature import CommandFeature
-        with Vimba.get_instance(), self.camera:
-            print('self.camera.get_all_features()')
-            return [f.get_name() for f in self.camera.get_all_features()
-                    if (1 in f.get_flags()) and (2 in f.get_flags())]
+        print('self.camera.get_all_features()')
+        return [f.get_name() for f in self.camera.get_all_features()
+                if (1 in f.get_flags()) and (2 in f.get_flags())]
 
     def get_attribute(self, name):
-        with Vimba.get_instance(), self.camera:
-            print('get_feature_by_name(name).get()')
-            return str(self.camera.get_feature_by_name(name).get())
+        print('get_feature_by_name(name).get()')
+        return str(self.camera.get_feature_by_name(name).get())
 
     def snap(self):
         '''Acquire a single image in manual mode and return it'''
@@ -54,23 +54,21 @@ class AlliedVisionCamera(object):
 
     def grab(self):
         '''Acquire a single image and return it'''
-        with Vimba.get_instance(), self.camera:
-            print('get_frame()')
-            frame = self.camera.get_frame()
+        print('get_frame()')
+        frame = self.camera.get_frame()
         return self._decode_image_data(frame)
 
     def grab_multiple(self, n_images, images, waitForNextBuffer=True):
         '''Grab n_images into images array during buffered acquistion.'''
 
         print(f"Acquiring from camera {n_images} images.")
-        with Vimba.get_instance(), self.camera:
-            image_number = 0
-            print('get_frame_generator(limit=n_images, timeout_ms=60*60*1000):')
-            for frame in self.camera.get_frame_generator(limit=n_images, timeout_ms=60*60*1000):
-                image = self._decode_image_data(frame)
-                print(f"    {image_number}: Acquire complete")
-                image_number += 1
-                images.append(image)
+        image_number = 0
+        print('get_frame_generator(limit=n_images, timeout_ms=60*60*1000):')
+        for frame in self.camera.get_frame_generator(limit=n_images, timeout_ms=60*60*1000):
+            image = self._decode_image_data(frame)
+            print(f"    {image_number}: Acquire complete")
+            image_number += 1
+            images.append(image)
 
     def stop_acquisition(self):
         pass
@@ -83,24 +81,10 @@ class AlliedVisionCamera(object):
         return frame.as_numpy_ndarray()[:,:,0]
 
     def close(self):
-        pass
+        self.camera.__exit__()
+        self.vimba.__exit__()
 
 class AlliedVisionCameraWorker(IMAQdxCameraWorker):
     # N.B. We use the camera server architecture from IMAQdxCamera,
     # but the interface is entirely based on the Allied Vision API.
-    def get_camera(self):
-        # Allied Vision firewire cameras do not reliably set attributes,
-        # so we will hide the manual mode attributes and only apply them
-        # when actually using the camera in manual mode.
-        self._manual_mode_attributes = self.manual_mode_camera_attributes
-        self.manual_mode_camera_attributes = self.camera_attributes
-
-        return AlliedVisionCamera(self.serial_number)
-
-    def snap(self):
-        self.set_attributes_smart(self._manual_mode_attributes)
-        super().snap()
-
-    def start_continuous(self, dt):
-        self.set_attributes_smart(self._manual_mode_attributes)
-        super().start_continuous(dt)
+    interface_class = AlliedVisionCamera
