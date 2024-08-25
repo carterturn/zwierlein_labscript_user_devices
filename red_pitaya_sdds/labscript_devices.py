f7f347a57b77a6e808c2787796af2b4a9def7ead
Carter Turn <carterturn@tutanota.de>
2024-08-25 11:37:12 -0400

    More numpy way of generating command array.
    Limiting factor is still ramp intersection checking

--- red_pitaya_sdds/labscript_devices.py
+++ red_pitaya_sdds/labscript_devices.py
@@ -44,31 +44,63 @@ class RedPitayaSDDSChannel(TriggerableDevice):
                     # Add 1e-9 (smaller than possible resolution) to mitigate floating point errors.
                     raise LabscriptError('%s requires trigger at %s, overlapping with a ramp from t = %s to %s' % (self.name, str(cl_[0]), str(t_start), str(t_end)))
 
-        conv_commands = []
-        for c in self.commands:
-            step_total = int(c[5] * self.FPGA_CLOCK)
-            conv_commands.append((c[1]*self.FREQ_SCALE,
-                                  (c[2]-c[1])*self.FREQ_SCALE*self.FPGA_STEP_SCALE/step_total,
-                                  c[3]*self.AMP_SCALE,
-                                  (c[4]-c[3])*2*self.AMP_SCALE*self.FPGA_STEP_SCALE/step_total,
-                                  step_total))
+        cmd_arr_float = np.array(self.commands,
+                                 dtype=[('t', float),
+                                        ('freq_start', float),
+                                        ('freq_stop', float),
+                                        ('amp_start', float),
+                                        ('amp_stop', float),
+                                        ('duration', float),
+                                        ('trigger', '<i1')])
 
-        command_array = np.array(conv_commands,
-                                 dtype=[('freq_start', '<u4'),
-                                        ('freq_step', '<i4'),
-                                        ('amp_start', '<u2'),
-                                        ('amp_step', '<i2'),
-                                        ('step_total', '<u4')])
+        cmd_arr_float_step = np.empty(cmd_arr_float.shape,
+                                      dtype=[('freq_start', float),
+                                             ('freq_step', float),
+                                             ('amp_start', float),
+                                             ('amp_step', float),
+                                             ('step_total', float),
+                                             ('trigger', '<i1'),
+                                             ('step_scale', '<i1')])
 
-        for i in range(len(self.commands)):
-            command_array['amp_start'][i] = (command_array['amp_start'][i]
-                                             | (0x0000 if self.commands[i][6] else 0x8000))
+        cmd_arr_float_step['step_total'] = cmd_arr_float['duration'] * self.FPGA_CLOCK
+        cmd_arr_float_step['freq_start'] = cmd_arr_float['freq_start']*self.FREQ_SCALE
+        cmd_arr_float_step['freq_step'] = ((cmd_arr_float['freq_stop'] - cmd_arr_float['freq_start'])
+                                           * self.FREQ_SCALE / cmd_arr_float_step['step_total'])
+        cmd_arr_float_step['amp_start'] = cmd_arr_float['amp_start'] * self.AMP_SCALE
+        cmd_arr_float_step['amp_step'] = ((cmd_arr_float['amp_stop'] - cmd_arr_float['amp_start'])
+                                          * 2*self.AMP_SCALE / cmd_arr_float_step['step_total'])
+        cmd_arr_float_step['trigger'] = cmd_arr_float['trigger']
+        # Check if we need to disable step scaling
+        cmd_arr_float_step['step_scale'] = np.logical_or(
+            np.logical_or(cmd_arr_float_step['freq_step']*2**16 > 2**31-1,
+                          cmd_arr_float_step['freq_step']*2**16 < -2**31),
+            np.logical_or(cmd_arr_float_step['amp_step']*2**16 > 2**15-1,
+                          cmd_arr_float_step['amp_step']*2**16 < -2**15))
+        # If we do, apply step scaling
+        cmd_arr_float_step['freq_step'] = (cmd_arr_float_step['freq_step']
+                                           * (2**16)**(cmd_arr_float_step['step_scale']==0))
+        cmd_arr_float_step['amp_step'] = (cmd_arr_float_step['amp_step']
+                                          * (2**16)**(cmd_arr_float_step['step_scale']==0))
+
+        cmd_arr_int = np.empty(cmd_arr_float_step.shape,
+                               dtype=[('freq_start', '<u4'),
+                                      ('freq_step', '<i4'),
+                                      ('amp_start', '<u2'),
+                                      ('amp_step', '<i2'),
+                                      ('step_total', '<u4')])
+
+        cmd_arr_int['freq_start'] = cmd_arr_float_step['freq_start']
+        cmd_arr_int['freq_step'] = cmd_arr_float_step['freq_step']
+        cmd_arr_int['amp_start'] = (cmd_arr_float_step['amp_start'].astype('<u2')
+                                   | 0x8000 * (cmd_arr_float_step['trigger']==0)
+                                   | 0x4000 * (cmd_arr_float_step['step_scale']==1))
+        cmd_arr_int['amp_step'] = cmd_arr_float_step['amp_step']
+        cmd_arr_int['step_total'] = cmd_arr_float_step['step_total']
 
         group = hdf5_file['devices'].require_group(self.parent_name)
-        print(group.keys())
-        group.create_dataset(self.channel, data=command_array)
+        group.create_dataset(self.channel, data=cmd_arr_int)
 
-    def ramp(self, t, duration, start_freq, stop_freq, start_amp, stop_amp):
+    def ramp(self, t, duration, start_freq, stop_freq, start_amp, stop_amp, trigger=True):
         '''
         Ramp frequency and amplitude from start values to end values over some time.
         Resolution is always hardware maximum (~1 MHz)
@@ -81,8 +113,9 @@ class RedPitayaSDDSChannel(TriggerableDevice):
         start_amp: Amplitude to start at, arbitrary units from 1.0 to 0.0
         stop_amp: Amplitude to stop at, arbitrary units from 1.0 to 0.0
         '''
-        self.commands.append((t, start_freq, stop_freq, start_amp, stop_amp, duration, True))
-        self.trigger(t=t, duration=duration/2.)
+        self.commands.append((t, start_freq, stop_freq, start_amp, stop_amp, duration, trigger))
+        if trigger:
+            self.trigger(t=t, duration=duration/2.)
 
     def constant(self, t, freq, amp):
         '''
@@ -93,7 +126,7 @@ class RedPitayaSDDSChannel(TriggerableDevice):
         freq: Frequency to set, Hz
         amp: Amplitude to set, arbitrary units from 1.0 to 0.0
         '''
-        self.commands.append((t, freq, freq, amp, amp, 0.0, True))
+        self.commands.append((t, freq, freq, amp, amp, 2e-6, True))
         self.trigger(t=t, duration=2e-6) # Need to be >1e-6s for safe triggering with NI card
 
     def customramp(self, t, duration, freq_function, amp_function, **kwargs):
